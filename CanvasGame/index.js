d561b74042de38f700b7d0b9b9ce321711a8e67f
Christopher Harris <56790192+decode-harris@users.noreply.github.com>
2021-01-28 03:23:02 +1100

    Magic Animation
    Created a "magic" action animation to character model. Will need to resize both png images in photoshop

--- CanvasGame/index.js
+++ CanvasGame/index.js
@@ -1,45 +1,60 @@
-// canvas game development
-
+// html canvas element
 const canvas = document.querySelector('#game');
+// method [ CanvasRenderingContext2D ]
 const ctx = canvas.getContext('2d');
 
 // canvas properties
 canvas.width = 800;
 canvas.height = 500;
 
-// array [ keys ] ''
+// array [ keys ]
 let keys = [];
 
-let keystrokes;
 // object [ player ] properties
 const player = {
+
+    // start position
     x: 1,
     y: 100,
 
-    // resize image in photoshop 
+    // dimensions [ resize image in photoshop ]
     width: 402,
     height: 343,
 
-    frameX: 1,
+    // animation frames
+    frameX: 12,
     frameY: 1,
 
+    // animation speed
     speed: -5,
 
+    // animation action
     moving: false,
 
 };
+// object [ action ] properties
+const action = {
 
+    // animation frames
+    frameX: 12,
+    frameY: 1,
+
+    // dimensions [ resize image in photoshop ]
+    width: 618,
+    height: 391,
+}
 
 // animated player element
 const playerSprite = new Image();
 playerSprite.src = 'wizard.png';
 
+// animated action element
+const playerAction = new Image();
+playerAction.src = 'magic.png';
+
 // animated background element
 const background = new Image();
 background.src = 'forest.png';
-
-// position variable
-// let position = 0;
 
 // function [ drawSprite ]
 function drawSprite(img, sourceX, sourceY, sourceWidth, sourceHeight, destinationX, destinationY, destinationWidth, destinationHeight) {
@@ -52,33 +67,55 @@ function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
 
-    drawSprite(playerSprite, 0, 0, player.width, player.height, player.x, player.y, player.width, player.height);
+    if (keys[0] == 's') {
 
+        // reset player canvas
+        ctx.clearRect(0, 0, player.width, player.height);
+
+        // redraw background behind new image
+        ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+        // draw the action animation
+        drawSprite(playerAction, 0, 0, action.width, action.height, player.x, player.y, action.width, player.height)
+        
+        // test directions
+        console.log('SHOOT : ' + keys[0]);
+    }
+    else {
+
+        // draw the player animation
+        drawSprite(playerSprite, 0, 0, player.width, player.height, player.x, player.y, player.width, player.height);
+    
+    }
+
+    // init [ movePlayer ] function
     movePlayer();
-    // position++;
 
-
+    // callback [ animate ] function
     requestAnimationFrame(animate);
-}
 
+}
 // init [ animate ] function
 animate();
 
-// console.log(playerSprite);
-
-
+// event [ keydown ] : add array keystroke
 window.addEventListener('keydown', function(e) {
     
     // remove last array item
     keys.pop(e.key);
 
-    // add current keypush to array
+    // add current keystroke to keys array
     keys.push(e.key);
     
     // test
-    console.log('keydown : ' + keys);
+    console.log('keydown : ' + keys[0]);
+
+    // output player position
+    console.log('PLAY POS [ X ] : ' + player.x);
+    console.log('PLAY POS [ y ] : ' + player.y);
 
 });
+// event [ keyup ] : delete array keystroke
 window.addEventListener('keyup', function(e) {
 
     delete keys[e.key];
@@ -91,20 +128,18 @@ window.addEventListener('keyup', function(e) {
 
 });
 
+// function [ movePlayer ] : directional player movement
 function movePlayer() {
 
+    // default character movement
     player.moving = false;
-
-    // console.log(player.entries);
-
-
+    
     // validate arrow keys
     if (keys[0] == 'ArrowUp') {
         
         // move player along the [ Y ] axis
         player.y += player.speed;
 
-        
         // test directions
         console.log('MOVE UPWARDS : ' + keys);
     }
@@ -116,7 +151,6 @@ function movePlayer() {
         // test directions
         console.log('MOVE DOWNWARDS : ' + keys[0]);
     }
-
     if (keys[0] == 'ArrowRight') {
 
         // move player along the [ x ] axis in a forward direction
@@ -124,7 +158,6 @@ function movePlayer() {
         // test directions
         console.log('MOVE RIGHT : ' + keys[0]);
     }
-
     if (keys[0] == 'ArrowLeft') {
 
         // move player along the [ x ] axis in a backwards direction
@@ -133,13 +166,16 @@ function movePlayer() {
         console.log('MOVE LEFT : ' + keys[0]);
     }
 
+    // if (keys[0] == 's') {
+
+    //     // ctx.clearRect(0, 0, player.width, player.height);
+    //     // drawSprite(playerMagic, 0, 0, player.width, player.height, player.x, player.y, player.width, player.height)
+    //     // move player along the [ x ] axis in a backwards direction
+    //     // player.x += player.speed;
+    //     // test directions
+    //     console.log('SHOOT : ' + keys[0]);
+    // }
     
-
-
-    
-
-    
-
     // test [ keys ] array output
     // console.log(keys);
 
