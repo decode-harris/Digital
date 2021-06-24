ee666ce5ef73a3c1f1021248bbd7effef6aa0886
Christopher Harris <56790192+decode-harris@users.noreply.github.com>
2021-06-24 16:31:15 +1000

    Wireframe : Mobile Layout
    Completed wireframe CSS layout. Need to fill empty sections with content

--- wireframe/index.js
+++ wireframe/index.js
@@ -96,14 +96,25 @@ more.addEventListener('click', ()=> {
 });
 
 
+
+// main ul [ items ] : project showcase + links
 let items = document.querySelectorAll('.main .items');
 console.log(items);
 
+// forEach [ items ] : 
 items.forEach(element => {
     element.addEventListener('click', ()=> {
+
+        // display project background as default [ none ]
         element.style.background = 'none';
+
+        // validate project file
         if ( element.id = 'project-one') {
+
+            // select the link of the currect element
             let link = element.firstElementChild;
+
+            // apply the external page navigation to href [ projects.html ]
             link.href = 'projects.html';
         }
         if (element.style.background != 'black') {
@@ -116,3 +127,81 @@ items.forEach(element => {
         
     })
 });
+
+
+let nav_items = document.querySelectorAll('.navigation .items');
+
+nav_items.forEach(element => {
+    element.addEventListener('click', ()=> {
+
+        if (navigation.style.width != '0') {
+            navigation.style.width = '0';
+            // return menu icon back to default [ bars ]
+            menu_icon.classList = 'fas fa-bars';
+        }
+    });
+});
+
+
+
+// select the navigation [ menu ] button
+let menu = document.querySelector('#menu');
+
+// select the hidden [ navigation ] element
+let navigation = document.querySelector('.navigation');
+navigation.style.width = '0';
+
+let menu_icon = document.querySelector('#menu').firstElementChild;
+
+console.log(menu_icon + 'as menu icon');
+
+// click event [ menu ] button
+menu.addEventListener('click', ()=> {
+
+    if ( navigation.style.width != '100%') {
+        // apply a width style of 100% to the navigation element
+        navigation.style.width = '100%';
+        // assign menu icon class to close [ times ]
+        menu_icon.classList = 'fas fa-times';
+    }
+    else {
+        // return the navigation width to default [ 0% ]
+        navigation.style.width = '0';
+        // return menu icon back to default [ bars ]
+        menu_icon.classList = 'fas fa-bars';
+    }
+
+});
+
+
+let mode = document.querySelector('#mode');
+let mode_icon = document.querySelector('#mode').firstElementChild;
+let page = document.querySelector('.page');
+
+mode.addEventListener('click', ()=> {
+
+    
+
+    console.log(page);
+
+    if (mode.classList != 'btn mode-dark') {
+        // assign class name to mode button [ mode-dark ]
+        mode.classList = 'btn mode-dark';
+        // assign mode icon class name to [ toggle on ] icon value
+        mode_icon.classList = 'fas fa-toggle-on';
+        // assign page wrapper class name to [ darkmode ]
+        page.classList = 'page darkmode';
+    }
+    else {
+        // return mode button class name to default [ mode-light ]
+        mode.classList = 'btn mode-light';
+        // return mode icon class name to [ toggle off] default
+        mode_icon.classList = 'fas fa-toggle-off';
+        // return page wrapper class name to [ page ]
+        page.classList = 'page';
+
+
+
+    }
+
+})
